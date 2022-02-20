cb2683c6d3f4ea89ef96b7d4ce3dbaa73022dc82
niklauswetter <89773361+niklauswetter@users.noreply.github.com>
2022-02-19 18:34:03 -0700

    Update DownloadNFLData.R

--- DownloadNFLData.R
+++ DownloadNFLData.R
@@ -5,5 +5,6 @@ library(readr)
 Rushing <- read.csv("https://raw.githubusercontent.com/niklauswetter/NFL_DATA_CSVS/main/2021Rushing.csv")
 Receiving <- read.csv("https://raw.githubusercontent.com/niklauswetter/NFL_DATA_CSVS/main/2021Receiving.csv")
 
+#Start Editing Here
 head(Receiving)
 head(Rushing)
